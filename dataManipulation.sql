130411396f936e65a47affbd4d87f0dc3d93b127
Steven Vu <MY_NAME@example.com>
2022-05-13 17:47:33 -0700

    SELECT + DELETE done

---
+++ dataManipulation.sql
@@ -0,0 +1,42 @@
+-- Customers
+SELECT * FROM Customers;
+SELECT customerID FROM Customers;
+SELECT customerFirstName FROM Customers;
+SELECT customerLastName FROM Customers;
+SELECT customerDateOfBirth FROM Customers;
+SELECT customerTotalCost FROM Customers;
+
+DELETE FROM Customers WHERE customerID = :customerIDInput;
+
+-- Suppliers
+SELECT * FROM Suppliers;
+SELECT supplierID FROM Suppliers;
+SELECT supplierName FROM Suppliers;
+
+DELETE FROM Suppliers WHERE supplierID = :supplierIDInput;
+
+-- Goods
+SELECT * FROM Goods;
+SELECT itemID FROM Goods;
+SELECT goodPrice FROM Goods;
+SELECT goodLocationInStore FROM Goods;
+SELECT goodExpirationDate FROM Goods;
+SELECT supplierID FROM Goods;
+SELECT orderID FROM Goods;
+
+DELETE FROM Goods WHERE itemID = :itemIDInput;
+
+-- Orders
+SELECT * FROM Orders;
+SELECT orderID FROM Orders;
+SELECT orderPurchaseDate FROM Orders;
+SELECT customerID FROM Orders;
+
+DELETE FROM Orders WHERE orderID = :itemIDInput;
+
+-- SupplierGoods
+SELECT * FROM SupplierGoods;
+SELECT itemID FROM SupplierGoods;
+SELECT supplierID FROM SupplierGoods;
+
+DELETE FROM SupplierGoods WHERE itemID = :itemIDInput AND supplierID = :supplierID;
