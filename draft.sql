b951fd8e93b8d296a8f2086fd78fc1cf786231cd
blakebabb <leopard50x1@gmail.com>
2022-05-13 17:06:10 -0700

    insert into customers

--- draft.sql
+++ draft.sql
@@ -1,17 +1,20 @@
 -- Clear way for new tables
+SET FOREIGN_KEY_CHECKS = 0;
 drop table if exists Customers;
 drop table if exists Suppliers;
 drop table if exists Goods;
 drop table if exists Orders;
-drop table if exists SuppliersGoods;
+drop table if exists SupplierGoods;
+SET FOREIGN_KEY_CHECKS = 1;
 
+-- Table creation
 
 create table Customers (
     customerID int(11) auto_increment,
     customerFirstName varchar(35) not null,
     customerLastName varchar(35) not null,
     customerDateOfBirth date not null,
-    customerTotalCost decimal(38) not null,
+    customerTotalCost decimal(8, 2) not null,
     primary key (customerID)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -47,3 +50,12 @@ CREATE TABLE SupplierGoods (
     FOREIGN KEY (supplierID) REFERENCES Goods (itemID),
     FOREIGN KEY (itemID) REFERENCES Suppliers (supplierID)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Insert sample data
+
+INSERT INTO Customers (CUSTOMERFIRSTNAME, CUSTOMERLASTNAME, CUSTOMERDATEOFBIRTH, CUSTOMERTOTALCOST)
+VALUES ('Joe', 'Schmoe', '1983/8/23', 12.39);
+INSERT INTO Customers (CUSTOMERFIRSTNAME, CUSTOMERLASTNAME, CUSTOMERDATEOFBIRTH, CUSTOMERTOTALCOST)
+VALUES ('Sarah', 'Smith', '1999/12/3', 99.99);
+INSERT INTO Customers (CUSTOMERFIRSTNAME, CUSTOMERLASTNAME, CUSTOMERDATEOFBIRTH, CUSTOMERTOTALCOST)
+VALUES ('Bo', 'Schmoe', '2007/3/12', 12.39);
