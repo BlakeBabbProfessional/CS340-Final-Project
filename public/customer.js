938db4fa0339abfb412930963e586a624ac587e4
Blake Babb <babbb@flip3.engr.oregonstate.edu>
2022-05-19 17:06:34 -0700

    customer basic filtering!

---
+++ public/customer.js
@@ -0,0 +1,30 @@
+document.getElementById('entity-filter-button').addEventListener('click', () => {
+    let filterText = document.getElementById('entity-filter-input').value
+    let filterColumnOption = document.getElementById('filter-dropdown-customers').selectedIndex
+
+    if (!filterText) {
+        window.location.href = '/customer'
+        return
+    }
+
+    let filterColumn
+    switch (filterColumnOption) {
+        case 0:
+            filterColumn = "customerID"
+            break
+        case 1:
+            filterColumn = "customerFirstName"
+            break;
+        case 2:
+            filterColumn = "customerLastName"
+            break
+        case 3:
+            filterColumn = "customerDateOfBirth"
+            break
+        case 4:
+            filterColumn = "customerTotalCost"
+            break
+    }
+
+    window.location.href = `/customer/${filterColumn}/"${filterText}"`
+})
